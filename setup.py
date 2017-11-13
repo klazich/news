b5596775395b011230c0d9cf6635ce67c979c55a
Kevin Lazich <dev@kevinlazich.com>
2017-11-12 21:06:52 -0500

    add python setuptools files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='news',
+      version='1.0',
+      py_modules=['news'],
+      install_requires=['click',
+                        'psycopg2',
+                        'pygments',
+                        'tabulate'],
+      entry_points='''
+        [console_scripts]
+        question=news:cli
+        ''', )
