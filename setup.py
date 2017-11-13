7dc9fca34fd4af98e0ebc262d2df6ee174bf278f
Kevin Lazich <dev@kevinlazich.com>
2017-11-13 17:07:07 -0500

    version 1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='news',
-      version='1.0',
+      version='1.1',
       py_modules=['news'],
       install_requires=['click',
                         'psycopg2',
